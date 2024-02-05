d3232df291e588b8ddc6ec401fe4fde8e72be00a
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2024-02-05 14:00:46 +1100

    reorderable list

--- lib/TodoPage.dart
+++ lib/TodoPage.dart
@@ -1,3 +1,5 @@
+import "dart:ui";
+
 import "package:flutter/material.dart";
 
 class TodoPage extends StatefulWidget {
@@ -14,6 +16,67 @@ class _TodoPageState extends State<TodoPage> {
       appBar: AppBar(
         title: Text('To Do'),
       ),
+      body: ReorderableList(),
+    );
+  }
+}
+
+class ReorderableList extends StatefulWidget {
+  const ReorderableList({super.key});
+
+  @override
+  State<ReorderableList> createState() => _ReorderableListState();
+}
+
+class _ReorderableListState extends State<ReorderableList> {
+  final List<String> _items = ["String 1", "String 2"];
+  @override
+  Widget build(BuildContext context) {
+    final List<Card> cards = <Card>[
+      for (int i = 0; i < _items.length; i++)
+        Card(
+            key: Key('$i'),
+            color: Theme.of(context).cardColor,
+            child: SizedBox(
+                height: 80, child: Center(child: Text('${_items[i]}'))))
+    ];
+
+    Widget proxyDecorator(
+        Widget child, int index, Animation<double> animation) {
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          final double animValue = Curves.easeInOut.transform(animation.value);
+          final double elevation = lerpDouble(1, 6, animValue)!;
+          final double scale = lerpDouble(1, 1.02, animValue)!;
+          return Transform.scale(
+            scale: scale,
+            // Create a Card based on the color and the content of the dragged one
+            // and set its elevation to the animated value.
+            child: Card(
+              elevation: elevation,
+              color: cards[index].color,
+              child: cards[index].child,
+            ),
+          );
+        },
+        child: child,
+      );
+    }
+
+    return ReorderableListView(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      proxyDecorator: proxyDecorator,
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final String item = _items.removeAt(oldIndex);
+          _items.insert(newIndex, item);
+        });
+      },
+      children: cards,
     );
   }
 }
