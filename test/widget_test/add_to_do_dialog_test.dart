4550ee2911bf1a2e1be13aba6009989cdcba253a
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2024-02-09 14:58:03 +1100

    finish off all widget tests up to main page

--- test/widget_test/add_to_do_dialog_test.dart
+++ test/widget_test/add_to_do_dialog_test.dart
@@ -1,6 +1,7 @@
 // To Do Dialog Box Widget Test.
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo/data/to_do.dart';
 import 'package:flutter_todo/to_do_page/add_to_do_dialog.dart';
 
 Widget initWidget() {
@@ -36,6 +37,76 @@ void main() {
 
       // check for text input field.
       expect(find.byKey(const Key('addToDoTextField')), findsOneWidget);
+    });
+  });
+
+  group('test text field', () {
+    testWidgets('test changes reflect in given controller',
+        (WidgetTester tester) async {
+      // mock environment
+      TextEditingController testController = TextEditingController();
+      List<ToDo> mockToDoList = [];
+      mockCreateToDo() {
+        mockToDoList.add(ToDo(testController.text, false));
+      }
+
+      // generate test environment for tododialog
+      await tester.pumpWidget(
+        MaterialApp(
+          home: NewTodoDialog(
+            createToDo: mockCreateToDo,
+            controller: testController,
+          ),
+        ),
+      );
+
+      // check inital texteditingcontroller state.
+      expect(testController.text, '');
+      // check input to textfield updates given controller.
+      await tester.enterText(find.byType(TextField), 'new task todo');
+      expect(testController.text, 'new task todo');
+
+      // call create to do manually (test save button seperately).
+      mockCreateToDo();
+      // check mockToDoList is as expected.
+      expect(mockToDoList.length, 1);
+      expect(mockToDoList[0].action, 'new task todo');
+      expect(mockToDoList[0].completion, false);
+    });
+  });
+
+  group('test save button', () {
+    testWidgets('test save button calls given function with controller',
+        (WidgetTester tester) async {
+      // mock environment
+      TextEditingController testController = TextEditingController();
+      List<ToDo> mockToDoList = [];
+      mockCreateToDo() {
+        mockToDoList.add(ToDo(testController.text, false));
+      }
+
+      // generate test environment for tododialog
+      await tester.pumpWidget(
+        MaterialApp(
+          home: NewTodoDialog(
+            createToDo: mockCreateToDo,
+            controller: testController,
+          ),
+        ),
+      );
+
+      // manually change controller state (test textfield seperately).
+      expect(testController.text, '');
+      testController.text = 'new task todo';
+      expect(testController.text, 'new task todo');
+
+      // click the save button
+      await tester.tap(find.widgetWithText(MaterialButton, 'Save'));
+
+      // check mockToDoList is as expected.
+      expect(mockToDoList.length, 1);
+      expect(mockToDoList[0].action, 'new task todo');
+      expect(mockToDoList[0].completion, false);
     });
   });
 }
--- test/widget_test/to_do_card_test.dart
+++ test/widget_test/to_do_card_test.dart
@@ -1,6 +1,7 @@
 // To Do List Card Tests.
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo/data/to_do.dart';
 import 'package:flutter_todo/to_do_page/to_do_card.dart';
 
 Widget initWidget() {
@@ -40,6 +41,127 @@ void main() {
 
       // check for component rendering and sub-components
       expect(find.byKey(const Key('TDDeleteButton1')), findsOneWidget);
+    });
+  });
+
+  group('test checkbox functionality for ToDoCard', () {
+    testWidgets(
+        'Test completion checkbox interacts correctly with mock function',
+        (WidgetTester tester) async {
+      // mock environment
+      List<ToDo> mockTodoList = [
+        ToDo('test', false),
+        ToDo('test2', true),
+      ];
+      mockToggleCompletion(bool newStatus, int index) {
+        mockTodoList[index].completion = newStatus;
+      }
+
+      // create widget
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ToDoCard(
+            index: 1,
+            completion: mockTodoList[1].completion,
+            toggleCompletion: mockToggleCompletion,
+            action: mockTodoList[1].action,
+            deleteToDo: (int index) {},
+          ),
+        ),
+      );
+
+      // test initial condition
+      expect(mockTodoList[0].completion, false);
+      expect(mockTodoList[1].completion, true);
+
+      // click checkbox
+      await tester.tap(find.byType(Checkbox));
+
+      // test completion status has changed
+      expect(mockTodoList[0].completion, false);
+      expect(mockTodoList[1].completion, false);
+    });
+  });
+
+  group('test text decoration functionality for ToDoCard', () {
+    testWidgets('Test completion correctly reflects in textdecoration',
+        (WidgetTester tester) async {
+      // mock environment
+      List<ToDo> mockTodoList = [
+        ToDo('test', false),
+        ToDo('test2', true),
+      ];
+
+      // create widget
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              ToDoCard(
+                index: 0,
+                completion: mockTodoList[0].completion,
+                toggleCompletion: (bool completion, int index) {},
+                action: mockTodoList[0].action,
+                deleteToDo: (int index) {},
+              ),
+              ToDoCard(
+                index: 1,
+                completion: mockTodoList[1].completion,
+                toggleCompletion: (bool completion, int index) {},
+                action: mockTodoList[1].action,
+                deleteToDo: (int index) {},
+              ),
+            ],
+          ),
+        ),
+      );
+
+      // test strikethrough is applied to completion
+      expect(mockTodoList[1].completion, true);
+      final Text widget = tester.firstWidget(find.text('test2'));
+      expect(widget.style?.decoration, TextDecoration.lineThrough);
+
+      expect(mockTodoList[0].completion, false);
+      final Text widget2 = tester.firstWidget(find.text('test'));
+      expect(widget2.style?.decoration, TextDecoration.none);
+    });
+  });
+
+  group('test delete functionality for ToDoCard', () {
+    testWidgets('Test delete button interacts correctly with mock function',
+        (WidgetTester tester) async {
+      // mock environment
+      List<ToDo> mockTodoList = [
+        ToDo('test', false),
+        ToDo('test2', true),
+      ];
+      mockDeleteToDo(int index) {
+        mockTodoList.removeAt(index);
+      }
+
+      // create widget
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ToDoCard(
+            index: 1,
+            completion: mockTodoList[1].completion,
+            toggleCompletion: (bool completion, int index) {},
+            action: mockTodoList[1].action,
+            deleteToDo: mockDeleteToDo,
+          ),
+        ),
+      );
+
+      // test initial condition
+      expect(mockTodoList.length, 2);
+
+      // click checkbox
+      await tester.tap(find.byType(IconButton));
+
+      // test completion status has changed
+      expect(mockTodoList.length, 1);
+      expect(mockTodoList[0].action, 'test');
+      expect(mockTodoList[0].completion, false);
     });
   });
 }
